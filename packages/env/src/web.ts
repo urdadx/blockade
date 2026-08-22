1e7e27e789f3a3ca889cb258b9895a4c1d339efe
urdadx <nerdyshinobi12@gmail.com>
2026-08-22 23:39:53 +0000

    removed server url

--- packages/env/src/web.ts
+++ packages/env/src/web.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 export const env = createEnv({
   clientPrefix: "VITE_",
   client: {
-    VITE_SERVER_URL: z.url(),
+    VITE_SERVER_URL: z.url().optional(),
     VITE_SENTRY_DSN: z.url().optional(),
   },
   runtimeEnv: (import.meta as any).env,
