cc6648c9a5abb19c55e387e6b4a92baee9dfe3e1
urdadx <nerdyshinobi12@gmail.com>
2026-09-18 15:43:17 +0000

    fix invalid checkmark icon height

--- apps/web/src/assets/icons/checkmark-icon.tsx
+++ apps/web/src/assets/icons/checkmark-icon.tsx
@@ -6,7 +6,7 @@ export function CheckMarkIcon(props: SVGProps<SVGSVGElement>) {
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width="1.5em"
-			height="1.5emem"
+			height="1.5em"
 			viewBox="0 0 24 24"
 			{...props}>
 			<path
